723241ccb9cf34067f92db6efd96421c10662a74
Josef Shainskiy <josef2011@gmail.com>
2021-09-04 15:11:16 +0300

    first commit

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {City} from "../models/cities";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {environment} from "../../environments/environment";
+import {WeatherResponse} from "../models/weatherResponceInterface";
+import {catchError, retry} from "rxjs/operators";
+import {throwError} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +13,24 @@ export class ApiService {
   constructor(private http: HttpClient) { }
   getWeather(id:any) {
     const url=`http://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${environment.API}&units=metric`;
-    return this.http.get<any>(url);
+    return this.http.get<WeatherResponse>(url)
+      .pipe(
+        retry(3), // retry a failed request up to 3 times
+        catchError(this.handleError) // then handle the error
+      );
+  }
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      'Something bad happened; please try again later.');
   }
 }
